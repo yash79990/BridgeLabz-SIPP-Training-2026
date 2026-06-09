f5311c917498436098b87671ccb08c8f3370d025
yash79990 <yash.awasthi_cs23@gla.ac.in>
2026-06-09 10:33:02 +0530

    [yash]add: convert km to miles

---
+++ core-java-practice/gcr-codebase/programming-elements/java programming level 1/harryage.java
@@ -0,0 +1,10 @@
+//Write a program to find the age of Harry if the birth year is 2000. Assume the Current Year is 2024
+//I/P => NONE
+//O/P => 
+class Main {
+    public static void main(String[] args) {
+        int current=2024;
+		int age=200;
+		System.out.println(current-age);
+    }
+}
