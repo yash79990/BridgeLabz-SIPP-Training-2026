9971cfe042b370d6cced945b4b718262e4889739
yash79990 <yash.awasthi_cs23@gla.ac.in>
2026-06-17 10:23:15 +0530

    Added: Leetcode21 Updated

---
+++ core-java-practice/leet-code-codebase/leetcode21.java
@@ -0,0 +1,50 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        if (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                list1.next = mergeTwoLists(list1.next, list2);
+                return list1;
+            } else {
+                list2.next = mergeTwoLists(list1, list2.next);
+                return list2;
+            }
+        }
+        if (list1 == null)
+            return list2;
+        return list1;
+    }
+}
+
+public class leetcode21 {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        Solution solution = new Solution();
+        ListNode merged = solution.mergeTwoLists(l1, l2);
+
+        while (merged != null) {
+            System.out.print(merged.val);
+            if (merged.next != null) {
+                System.out.print(" -> ");
+            }
+            merged = merged.next;
+        }
+        System.out.println();
+    }
+}
